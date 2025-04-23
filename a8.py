3538914fda0baf6ff6f7c2390549dc3936e7d2fb
lyogics <lyogi@cps.edu>
2025-04-23 11:41:44 -0500

    finished xor neural nets

--- a8.py
+++ a8.py
@@ -2,10 +2,16 @@ from neural import *
 
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
-xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
+xor_training_data = [([0, 0], [0]), ([0, 1], [1]), ([1, 0], [1]), ([1, 1], [0])]
 
 xorn = NeuralNet(2, 2, 1)
-xorn.train(xor_training_data, learning_rate=.7, iters=1000, print_interval=1000)
+xorn.train(xor_training_data, learning_rate=.7, iters=10000, print_interval=1000)
 print(xorn.test_with_expected(xor_training_data))
 
+xorn = NeuralNet(2, 8, 1)
+xorn.train(xor_training_data, iters=10000, print_interval=500)
+print(xorn.test_with_expected(xor_training_data))
 
+xorn = NeuralNet(2, 1, 1)
+xorn.train(xor_training_data, learning_rate=.7, iters=10000, print_interval=1000)
+print(xorn.test_with_expected(xor_training_data))
