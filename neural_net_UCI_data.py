13906bd96b9a1139ae586af0ce2afc1a0c8d1943
lyogics <lyogi@cps.edu>
2025-04-25 11:31:40 -0500

    added

--- neural_net_UCI_data.py
+++ neural_net_UCI_data.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 from neural import *
-
+from sklearn.model_selection import train_test_split
 
 def parse_line(line: str) -> Tuple[List[float], List[float]]:
     """Splits line of CSV into inputs and output (transormfing output as appropriate)
@@ -48,16 +48,26 @@ def normalize(data: List[Tuple[List[float], List[float]]]):
 with open("wine_data.txt", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
 
-#for line in training_data:
+train_data_split, test_data_split = train_test_split(training_data)
+
+for line in training_data:
+    print(line)
+
+print(len(training_data))
+print(len(train_data_split))
+print(len(test_data_split))
+
+#td = normalize(training_data)
+train_data_norm = normalize(train_data_split)
+test_data_norm = normalize(test_data_split)
+
+#for line in test_data_norm:
    # print(line)
-
-td = normalize(training_data)
-
 #for line in td:
    # print(line)
 
 nn = NeuralNet(13, 3, 1)
-nn.train(td, iters=10000, print_interval=1000, learning_rate=0.1)
+nn.train(train_data_norm, iters=10000, print_interval=1000, learning_rate=0.5)
 
-for i in nn.test_with_expected(td):
+for i in nn.test_with_expected(test_data_norm):
     print(f"desired: {i[1]}, actual: {i[2]}")
