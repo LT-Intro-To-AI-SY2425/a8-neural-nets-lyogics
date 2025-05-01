dad512b38942e61884aae3d577f342cdc5d2792f
lyogics <lyogi@cps.edu>
2025-05-01 11:32:17 -0500

    done work

--- neural_net_UCI_data.py
+++ neural_net_UCI_data.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 from neural import *
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 
 def parse_line(line: str) -> Tuple[List[float], List[float]]:
     """Splits line of CSV into inputs and output (transormfing output as appropriate)
@@ -12,7 +12,7 @@ def parse_line(line: str) -> Tuple[List[float], List[float]]:
         tuple of input list and output list
     """
     tokens = line.split(",")
-    out = int(tokens[0])
+    out = (tokens[0])
     output = [0 if out == 1 else 0.5 if out == 2 else 1]
 
     inpt = [float(x) for x in tokens[1:]]
@@ -48,26 +48,26 @@ def normalize(data: List[Tuple[List[float], List[float]]]):
 with open("wine_data.txt", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
 
-train_data_split, test_data_split = train_test_split(training_data)
+# train_data_split, test_data_split = train_test_split(training_data)
 
-for line in training_data:
-    print(line)
+# for line in training_data:
+#     print(line)
 
-print(len(training_data))
-print(len(train_data_split))
-print(len(test_data_split))
+# print(len(training_data))
+# print(len(train_data_split))
+# print(len(test_data_split))
 
-#td = normalize(training_data)
-train_data_norm = normalize(train_data_split)
-test_data_norm = normalize(test_data_split)
+# #td = normalize(training_data)
+# train_data_norm = normalize(train_data_split)
+# test_data_norm = normalize(test_data_split)
 
-#for line in test_data_norm:
-   # print(line)
-#for line in td:
-   # print(line)
+# #for line in test_data_norm:
+#    # print(line)
+# #for line in td:
+#    # print(line)
 
-nn = NeuralNet(13, 3, 1)
-nn.train(train_data_norm, iters=10000, print_interval=1000, learning_rate=0.5)
+# nn = NeuralNet(13, 3, 1)
+# nn.train(train_data_norm, iters=10000, print_interval=1000, learning_rate=0.5)
 
-for i in nn.test_with_expected(test_data_norm):
-    print(f"desired: {i[1]}, actual: {i[2]}")
+# for i in nn.test_with_expected(test_data_norm):
+#     print(f"desired: {i[1]}, actual: {i[2]}")
--- neural_net_abalone.py
+++ neural_net_abalone.py
@@ -1,32 +1,31 @@
 from typing import Tuple
-from neural import *
+from neural import NeuralNet
 from neural_net_UCI_data import parse_line
-from neural_net_UCI_data import normalize
-from sklearn.model_selection import train_test_split
+
 
 with open("abalone_data.txt", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
 
-train_data_split, test_data_split = train_test_split(training_data)
+abann = NeuralNet(8, 0, 1)
+abann.train(training_data, learning_rate=0.7, iters=10000, print_interval=1000)
 
-for line in training_data:
-    print(line)
+# F = 0.1, M = 0.3, I = 0.2
+test_data = [
+    ([0.2, 0.123, 0.941, 0.322, 0.632, 0.101, 0.021, 0.91], [20]),
+    ([0.3, 0.32, 0.18, 0.291, 0.234, 0.41, 0.01, 0.72], [8]),
+    ([0.1, 0.302, 0.148, 0.698, 0.251, 0.28, 0.61, 0.82], [15]),
+    ([0.1, 0.42, 0.186, 0.421, 0.765, 0.76, 0.53, 0.28], [6]),
+    ([0.3, 0.27, 0.671, 0.931, 0.892, 0.522, 0.19, 0.219], [17]),
+    ([0.2, 0.189, 0.722, 0.367, 0.124, 0.401, 0.13, 0.92], [12]),
+    ([0.3, 0.82, 0.09, 0.458, 0.41, 0.51, 0.954, 0.48], [9]),
+    ([0.3, 0.96, 0.102, 0.261, 0.62, 0.472, 0.321, 0.12], [10]),
+    ([0.2, 0.34, 0.22, 0.212, 0.89, 0.195, 0.745, 0.732], [7]),
+    ([0.1, 0.75, 0.31, 0.875, 0.13, 0.839, 0.987, 0.42], [13])
+    ]
 
-print(len(training_data))
-print(len(train_data_split))
-print(len(test_data_split))
 
-#td = normalize(training_data)
-train_data_norm = normalize(train_data_split)
-test_data_norm = normalize(test_data_split)
-
-#for line in test_data_norm:
-   # print(line)
-#for line in td:
-   # print(line)
-
-nn = NeuralNet(13, 3, 1)
-nn.train(train_data_norm, iters=10000, print_interval=1000, learning_rate=0.5)
-
-for i in nn.test_with_expected(test_data_norm):
+nn = NeuralNet(8, 0, 1)
+nn.train(training_data, iters=10000, print_interval=1000, learning_rate=0.5)
+nn.evaluate([0.2, 0.123, 0.941, 0.322, 0.632, 0.101, 0.021, 0.91])
+for i in nn.test_with_expected(test_data):
     print(f"desired: {i[1]}, actual: {i[2]}")   
